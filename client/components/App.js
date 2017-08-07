d958b0b78d2d73d732b0037c6e01ca0f0489b87d
James <smythie_89@hotmail.com>
2017-08-07 20:27:21 +0400

    Started on the main circle

--- client/components/App.js
+++ client/components/App.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class Initial extends Component {
   render() {
     return (
-      <div className="atmosphere">
-        Atmosphere initial build
+      <div className="circle">
+        <div className="segment"></div>
       </div>
     )
   }
