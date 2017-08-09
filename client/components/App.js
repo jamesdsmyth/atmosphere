bb6036f99c27179e9f3f476a05c20323191cb31a
James <smythie_89@hotmail.com>
2017-08-09 12:09:27 +0400

    API call initiated and retrieving the true values

--- client/components/App.js
+++ client/components/App.js
@@ -3,14 +3,12 @@ import React, { Component } from 'react';
 export default class Initial extends Component {
 
   apiCall() {
-    
-    return new Promise((resolve, reject) => {
-      var myHeaders = new Headers();
-      fetch('http://api.openweathermap.org/data/2.5/forecast?q=London,&mode=json&appid=fb161b8bdfd1a946ed269b0b2cf42b77').then((response) => {
-        return response.json();
-      }).then((json) => {
-        console.log(json);
-      });
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=London,&mode=json&appid=fb161b8bdfd1a946ed269b0b2cf42b77').then((response) => {
+      return response.json();
+    }).then((json) => {
+      console.log(json);
+    }).catch((error) => {
+      console.log(error);
     });
   }
 
