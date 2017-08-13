6c2863e76ed02881777025c9289dd3357fd514d9
James <smythie_89@hotmail.com>
2017-08-13 13:53:27 +0400

    Created the clock

---
+++ client/components/Clock.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+
+export default class Clock extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      loaded: false
+    }
+  }
+
+  time() {
+    setInterval(this.getTime.bind(this), 1000);
+  }
+
+  getTime() {
+    let currentTime = moment(new Date()).format('H:mm');
+
+    this.setState({
+      currentTime: currentTime,
+      loaded: true
+    });
+
+  }
+
+  render() {
+
+    {
+      this.state.loaded === false && this.time();
+      console.log('here');
+    }
+
+    return (
+      <time className="time">
+        {this.state.currentTime}
+      </time>
+    )
+  }
+}
