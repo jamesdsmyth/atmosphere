b3abe485d4379c6ce44ef64ae5a0e77e978d73fa
James <smythie_89@hotmail.com>
2017-08-14 21:15:29 +0400

    Adding the colors in a case statment

--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -2,6 +2,57 @@ import React, { Component } from 'react';
 
 export default class Canvas extends Component {
 
+  color(t) {
+    let color = null;
+
+    console.log(t);
+
+    switch(parseInt(t)) {
+      case (t < 5):
+        color = '#2f358f';
+        break;
+      
+      case (t < 10):
+        color = '#0a56a2';
+        break;
+
+      case (t < 15):
+        color = '#1aa79d';
+        break;
+
+      case (t < 20):
+        color = '#17a456';
+        break;
+
+      case (t < 25):
+        color = '#fef036';
+        break;
+
+      case (t < 30):
+        color = '#fec42e';
+        break;
+
+      case (t < 35):
+        color = '#fd9326';
+        break;
+
+      case (t < 40):
+        color = '#f16631';
+        break;
+
+      case (t < 100):
+        color = '#eb212f';
+        break;
+
+      default: 
+        color = 'yellow';
+    } 
+
+    console.log(color);
+
+    return color;
+  }
+
   render() {
 
     let canvas = document.getElementById('canvas');
@@ -20,12 +71,18 @@ export default class Canvas extends Component {
       let ctx = canvas.getContext('2d');
       ctx.scale(2,2);
 
+      // ctx.arc(x, y, radius, startAngle in radian, endAngle in radian, anticlockwise);
+
+      // this loops through the 8 items in the array
       this.props.weatherArray.map((x, i) => {
+        let temperature = Math.round(x.main.temp - 273.15);
+        let color = this.color(temperature);
+        console.log(color);
         ctx.beginPath();
         ctx.moveTo(300,300);
-        ctx.fillStyle='rgb(' + Math.floor(255-42.5*i) + ', 95, 180)';
-        ctx.arc(300, 300, 150, (i * radian), ((i + 1) * radian), false);
+        ctx.arc(300, 300, 300, (i * radian), ((i + 1) * radian), false);
         ctx.closePath();
+        ctx.fillStyle=`${color}`;
         ctx.fill(); 
       });
     }
