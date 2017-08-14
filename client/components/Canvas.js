801dbd8358eb281b2d4db39ea7c47adaf023c33d
James <smythie_89@hotmail.com>
2017-08-14 10:59:44 +0400

    Added first segment using canvas

--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 
 export default class Canvas extends Component {
+
+  draw() {
+    let canvas = document.getElementById('canvas');
+    let w = 300;
+    let h = 300;
+
+    canvas.width = w * 2;
+    canvas.height = h * 2;
+    canvas.style.width = w + 'px';
+    canvas.style.height = h + 'px';
+
+    let ctx = canvas.getContext('2d')
+    
+    ctx.scale(2,2);
+
+    ctx.beginPath();
+    ctx.moveTo(150, 150);
+    ctx.lineTo(195, 0);
+    ctx.lineTo(105, 0);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+  componentDidMount() {
+    this.draw();
+  }
+
   render() {
+
     return (
-      <canvas className="canvas"></canvas>
+      <canvas id="canvas"></canvas>
     )
   }
 }
