abdf54f21ca929f9516dd4647b6ca9d0a6ca57a1
James <smythie_89@hotmail.com>
2017-08-14 09:36:39 +0400

    Created canvas, now to branch off

--- client/components/Circle.js
+++ client/components/Circle.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Segments from './Segments';
+import Canvas from './Canvas';
 import Time from './Time';
 
 const Circle = ( { weatherArray } ) => (
   <div className="circle-container">
-    <Time />
-    <Segments array={weatherArray} />
+    {/* <Time /> */}
+    {/* <Segments array={weatherArray} /> */}
+    <Canvas />
   </div>
 )
 
