034ee56e1abb31fa205102ee5e78adcefae9e57b
James <smythie_89@hotmail.com>
2017-08-15 09:58:58 +0400

    Colors now coming in properly for the canvas

--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -5,50 +5,59 @@ export default class Canvas extends Component {
   color(t) {
     let color = null;
 
+
+    // $blue: #2f358f; // 0 -5
+    // $light-blue: #0a56a2; // 6 - 10
+    // $light-green: #1aa79d; // 11 - 15
+    // $green: #17a456; // 16 - 20
+    // $yellow: #fef036; // 21 - 25
+    // $light-orange: #fec42e; // 26 - 30
+    // $orange: #fd9326; // 31 - 35
+    // $dark-orange: #f16631; // 36 - 40
+    // $red: #eb212f; // 41 - 50
+
     console.log(t);
 
-    switch(t) {
-      case (t < 5):
+    switch(true) {
+      case t < 5:
         color = '#2f358f';
         break;
       
-      case (t < 10):
+      case t <= 10:
         color = '#0a56a2';
         break;
 
-      case (t < 15):
+      case t <= 15:
         color = '#1aa79d';
         break;
 
-      case (t < 20):
+      case t <= 20:
         color = '#17a456';
         break;
 
-      case (t < 25):
+      case t <= 25:
         color = '#fef036';
         break;
 
-      case (t < 30):
+      case t <= 30:
         color = '#fec42e';
         break;
 
-      case (t < 35):
+      case t <= 35:
         color = '#fd9326';
         break;
 
-      case (t < 40):
+      case t <= 40:
         color = '#f16631';
         break;
 
-      case (t < 100):
+      case t <= 55:
         color = '#eb212f';
         break;
 
       default: 
-        color = 'yellow';
+        color = '#ffffff';
     }
-
-    console.log(color);
 
     return color;
   }
@@ -61,7 +70,7 @@ export default class Canvas extends Component {
 
       let w = 600;
       let h = 600;
-      let radian = 0.785398;
+      let radian = 0.785398; // 45 deg
 
       canvas.width = w * 2;
       canvas.height = h * 2;
@@ -76,8 +85,9 @@ export default class Canvas extends Component {
       // this loops through the 8 items in the array
       this.props.weatherArray.map((x, i) => {
         let temperature = Math.round(x.main.temp - 273.15);
+
+        temperature = parseInt(temperature);
         let color = this.color(temperature);
-        console.log(color);
         ctx.beginPath();
         ctx.moveTo(300,300);
         ctx.arc(300, 300, 300, (i * radian), ((i + 1) * radian), false);
