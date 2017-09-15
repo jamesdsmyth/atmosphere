30e52522c4bdbd4d9d7fddd3cf7a396cd16ae1e1
James <smythie_89@hotmail.com>
2017-09-15 13:22:49 +0400

    Updated promise

--- client/api/Api.js
+++ client/api/Api.js
@@ -3,36 +3,30 @@ import populateWeather from '../actions/Actions';
 
 const apiCall = () => {
 
-  const call = () => {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition((position) => {
+  const callPromise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition((position) => {
 
-        let ceilingLat = Math.floor(position.coords.latitude);
-        let ceilingLng = Math.floor(position.coords.longitude);
-        let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${ceilingLat}&lon=${ceilingLng},&mode=json&appid=fb161b8bdfd1a946ed269b0b2cf42b77`;
+      let ceilingLat = Math.floor(position.coords.latitude);
+      let ceilingLng = Math.floor(position.coords.longitude);
+      let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${ceilingLat}&lon=${ceilingLng},&mode=json&appid=fb161b8bdfd1a946ed269b0b2cf42b77`;
 
-        fetch(url).then((response) => {
-          return response.json();
-        }).then((json) => {
-          resolve(json);
-        }).catch((error) => {
-          reject(error);
-        });
+      fetch(url).then((response) => {
+        return response.json();
+      }).then((json) => {
+        resolve(json);
+      }).catch((error) => {
+        reject(error);
       });
     });
-  }
+  });
 
-  call().then((response) => {
+  
+  // callPromise is the promise thar if resolved/rejected when deal with it below
+  callPromise.then((response) => {
     Store.dispatch(populateWeather(response));
-    console.log(response);
   }, (error) => {
     alert(error);
   });
 }
 
 export default apiCall;
-
-// promise here tht resolves if the url is correct.
-// call() takes two arguments which is a resolve or a reject function.
-// within call() we have a promie that takes both these arguments
-// we use fetch to call the api and depending on that we call resolve or reject
