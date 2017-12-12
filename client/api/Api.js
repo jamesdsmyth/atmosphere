33cfa9a116a6e196b7b91f12a3cf595fcdcaa68e
James <smythie_89@hotmail.com>
2017-12-12 17:46:07 +0400

    Updated packages

--- client/api/Api.js
+++ client/api/Api.js
@@ -4,25 +4,24 @@ import populateWeather from '../actions/Actions';
 const apiCall = () => {
 
   const callPromise = new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition(position => {
 
       let ceilingLat = Math.floor(position.coords.latitude);
       let ceilingLng = Math.floor(position.coords.longitude);
       let url = `http://api.openweathermap.org/data/2.5/forecast?lat=${ceilingLat}&lon=${ceilingLng},&mode=json&appid=fb161b8bdfd1a946ed269b0b2cf42b77`;
 
-      fetch(url).then((response) => {
+      fetch(url).then(response => {
         return response.json();
-      }).then((json) => {
+      }).then(json => {
         resolve(json);
-      }).catch((error) => {
+      }).catch(error => {
         reject(error);
       });
     });
-  });
-
+  }); 
   
   // callPromise is the promise thar if resolved/rejected when deal with it below
-  callPromise.then((response) => {
+  callPromise.then(response => {
     Store.dispatch(populateWeather(response));
   }, (error) => {
     alert(error);
